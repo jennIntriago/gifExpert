2e9afaa6d7e2f7d910b916bcea86e68153f76583
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-08-27 11:21:30 -0500

    archivos de barril

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,11 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GrifGid } from "./components/GrifGid";
+import { AddCategory, GrifGid } from "./components";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch"]);
   const onAddCategory = (newCategory) => {
-    //Preguntamos si la categoria existe. Si existe no hace nada, no lo inserta, si no existe lo inserta
     if (categories.includes(newCategory)) return;
     setCategories([newCategory, ...categories]);
-    // setCategories((cat) => [...cat, "Valorant"]);
   };
   return (
     <>
--- src/components/GrifGid.jsx
+++ src/components/GrifGid.jsx
@@ -8,6 +8,8 @@ export const GrifGid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
+
       <div className="card-grid">
         {images.map((image) => {
           return <GifItem key={image.id} {...image} />;
