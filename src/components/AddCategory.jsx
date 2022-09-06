d0cd5444a60a340c054c8bce1603ab1d45d39846
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-09-06 17:26:00 -0500

    pruebas con el formulario

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -15,7 +15,7 @@ export const AddCategory = ({ onNewCategory }) => {
     setInputValue("");
   };
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} aria-label="form">
       <input
         type="text"
         placeholder="Buscar gif"
--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -7,6 +7,19 @@ describe("Pruebas en <AddCategory />", () => {
     const input = screen.getByRole("textbox");
     fireEvent.input(input, { target: { value: "Saitama" } });
     expect(input.value).toBe("Saitama");
-    screen.debug();
+    // screen.debug();
+  });
+
+  test("debe de llamar onNewCategory si el input tiene un valor", () => {
+    const inputValue = "Saitama";
+    render(<AddCategory onNewCategory={() => {}} />);
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+    // screen.debug();
+
+    expect(input.value).toBe("");
   });
 });
